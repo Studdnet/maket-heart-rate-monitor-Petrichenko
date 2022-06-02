a59d55de2591e1acbf3f8233a2a05afd6e44c530
Stud.net <st.codding@gmail.com>
2022-06-02 22:46:49 +0300

    finish section {carousel}

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 1200,
-		adaptiveHeight: true,
-		autoplay: true,
+		autoplay: false,
 		autoplaySpeed: 1200,
 		prevArrow: '<button type="button" class="slick-prev"><img src="/icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="/icons/right.svg"></button>',
 		responsive: [
 			{
 			  breakpoint: 768,
+			  adaptiveHeight: true,
 			  settings: {
 				 dots: true,
 				 arrows: false
