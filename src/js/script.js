03c70e0ba33314857a079bf36b36397342a6087b
Stud.net <st.codding@gmail.com>
2022-06-09 00:08:38 +0300

    add {modal windows}

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+	//slick-slider
+
 	$('.carousel__inner').slick({
 		speed: 1200,
 		autoplay: false,
@@ -15,15 +18,17 @@ $(document).ready(function () {
 		}, ]
 	});
 
+	//Slide of card
+
 	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
 		$(this)
 			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
 			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 	});
 
-	function toggleSlide (item) {
-		$(item).each(function(i) {
-			$(this).on('click', function(e) {
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
 				e.preventDefault();
 				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
 				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
@@ -32,4 +37,29 @@ $(document).ready(function () {
 	};
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
+
+	// Modal windows 
+
+	$('[data-modal=consultation]').on('click', function () {
+		$('body').addClass('_lock');
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+
+	$('.modal__close').on('click', function () {
+		$('body').removeClass('_lock');
+		$('.overlay, #consultation, #order, #thanks').fadeOut();
+	});
+
+	$('.overlay').on('click', function () {
+		$('body').removeClass('_lock');
+		$('.overlay, #consultation, #order, #thanks').fadeOut();
+	});
+
+	$('.button_catalog').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
+
 });
